d63886d26f4dca5ee8cca6627394cf85d0173b84
Daniil <danilmux@gmail.com>
2024-09-02 23:59:21 +0500

    webpack config, css modules, ts init

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import "./index.scss";
+import { Counter } from "./src/components/Counter";
+
+const App = () => {
+  return (
+    <div className="app">
+      asdawdwadaw
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
