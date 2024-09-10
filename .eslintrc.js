bc28f28eb225ad3ed9db180ec6047ba05c696a00
Daniil <danilmux@gmail.com>
2024-09-10 14:24:49 +0500

    fix i18n markup for test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,11 +41,22 @@ module.exports = {
         'import/no-extraneous-dependencies': 'warn',
         'no-underscore-dangle': 'off',
         'no-unused-vars': 'warn',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': ['error', {
+            markupOnly: true,
+            ignoreAttribute: ['data-testid'],
+        }],
         'max-len': ['error', { code: 100, ignoreComments: true }],
         'no-console': 'off',
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
